7dbdc5fe418d5f5713ec5fcc01f9ddbad8ee7884
Cristhian Daza <cristian.d.b@hotmail.com>
2023-05-12 19:51:38 -0500

    test(tv-hero.spec.js - tv-hero.spec.js.snap) The snapshot is updated

--- tests/unit/component/tv-hero.spec.js
+++ tests/unit/component/tv-hero.spec.js
@@ -5,52 +5,16 @@ describe("TvButton Component", () => {
   it("should matches snapshot", () => {
     const wrapper = shallowMount(TvHero, {
       props: {
-        titleText: "TODOvue",
-        descriptionText: "TODOvue Blog",
-        buttonPrimaryText: "Primary",
-        buttonSecondaryText: "Secondary",
+        configHero: {
+          titleText: "TODOvue Blog",
+          descriptionText:
+            "Introducing my Vue.js blog! Get ready to dive into the world of Vue.js.",
+          buttonText: "View all blogs",
+          image: "https://todovue.com/logo.png",
+          alt: "TODOvue Logo",
+        },
       },
     });
     expect(wrapper.html()).toMatchSnapshot();
-  });
-
-  it("emits a click event when the button is clicked", () => {
-    const wrapper = shallowMount(TvHero);
-    wrapper.find("tv-button-stub").trigger("click");
-    expect(wrapper.emitted("click")).toBeTruthy();
-  });
-
-  it("emits 'clickPrimary' event when clickPrimary method is called", async () => {
-    const wrapper = shallowMount(TvHero);
-    await wrapper.vm.clickPrimaryButton();
-
-    expect(wrapper.emitted("clickPrimaryButton")).toBeTruthy();
-  });
-
-  it("emits 'clickSecondary' event when clickSecondary method is called", async () => {
-    const wrapper = shallowMount(TvHero);
-    await wrapper.vm.clickSecondaryButton();
-
-    expect(wrapper.emitted("clickSecondaryButton")).toBeTruthy();
-  });
-
-  it("renders the title correctly", () => {
-    const titleText = "Test title";
-    const wrapper = shallowMount(TvHero, {
-      propsData: {
-        image: "https://todovue.com/img/todovue-logo.png",
-        imageName: "Test image",
-        titleText: titleText,
-      },
-      slots: {
-        title: `<div>${titleText}</div>`,
-        image: `<img src="https://todovue.com/img/todovue-logo.png" alt="Test image" />`,
-      },
-    });
-    expect(wrapper.find(".tv-hero-title").text()).toBe(titleText);
-    expect(wrapper.find("img").attributes("alt")).toBe("Test image");
-    expect(wrapper.find("img").attributes("src")).toBe(
-      "https://todovue.com/img/todovue-logo.png"
-    );
   });
 });
