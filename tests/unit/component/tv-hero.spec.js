ea89f2b62ca3dedf31b88379f54f796f27344b45
Cristhian Daza <cristian.d.b@hotmail.com>
2023-05-15 14:58:41 -0500

    fix(tv-hero.spec.js) Fix test

--- tests/unit/component/tv-hero.spec.js
+++ tests/unit/component/tv-hero.spec.js
@@ -3,10 +3,10 @@ import TvHero from "@/component/TvHero.vue";
 
 describe("TvButton Component", () => {
   const configHero = {
-    titleText: "TODOvue Blog",
-    descriptionText:
+    title: "TODOvue Blog",
+    description:
       "Introducing my Vue.js blog! Get ready to dive into the world of Vue.js.",
-    buttonText: "View all blogs",
+    button: "View all blogs",
     image: "https://todovue.com/logo.png",
     alt: "TODOvue Logo",
   };
@@ -44,7 +44,7 @@ describe("TvButton Component", () => {
       props: {
         configHero: {
           ...configHero,
-          buttonSecondaryText: "View more",
+          buttonSecondary: "View more",
         },
       },
     });
@@ -57,7 +57,7 @@ describe("TvButton Component", () => {
       props: {
         configHero: {
           ...configHero,
-          buttonSecondaryText: "View last blog",
+          buttonSecondary: "View last blog",
         },
         customHero: {
           bgBody: "#1e1d23",
